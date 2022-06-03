547695f4a34fbde7005674f4c4545be66b86708c
sbaack <s.baack@posteo.de>
2022-06-03 11:46:11 +0200

    Add --bare option to default-which
    With this new option you get only the version number of the current
    default Python in pymac

--- commands/default-which.bash
+++ commands/default-which.bash
@@ -2,35 +2,49 @@
 
 help() {
   printf "Show path to Python version symlinked to ~/.config/pymac/default.\n\n"
-  printf "Usage: pymac default-which <Major.Minor version number>\n"
+  printf "Usage: pymac default-which [--bare]\n\n"
+  printf "Optional arguments:\n"
+  printf "  --bare: Only show version number of current default instead of full path\n"
 }
 
 default-which() {
+  local bare="$1"
   local default
   default=$(readlink ~/.config/pymac/default)
   if [[ -z "$default" ]]; then
     printf "No default set.\n"
   else
-    printf "%s/bin/python3\n" "$default"
+    if [[ $bare == true ]]; then
+      local version_num
+      version_num=$(basename "$default")
+      printf "%s\n" "$version_num"
+    else
+      printf "%s/bin/python3\n" "$default"
+    fi
   fi
 }
 
 parse_args() {
+  local bare=false
   while :; do
     case "$1" in
       "")
-        default-which
+        break
+        ;;
+      --bare)
+        bare=true
         break
         ;;
       -h|help|--help)
         help
-        break
+        return 0
         ;;
       *)
         printf "Invalid option. See 'pymac default help' for usage.\n"
         exit 1
     esac
   done
+ default-which "$bare" 
 }
 
 parse_args "$@"
